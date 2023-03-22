85846d251790634b4adb7133a187034e8e36bc1b
raihan.k.abdillah@outlook.com <raihan.k.abdillah@outlook.com>
2023-03-22 13:46:55 +0800

    Added some lines to information.cs

--- WikiApplication/Information.cs
+++ WikiApplication/Information.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace WikiApplication
 {
-    internal class Information
+    internal class Information: IComparable<Information>
     {
+        private string name;
+        private string category;
+        private string structure;
+        private string definition;
+
+        public Information(string newName, string newCategory, string newStructure, string newDefinition)
+        {
+            name = newName;
+            category = newCategory;
+            structure = newStructure;
+            definition = newDefinition;
+        }
+
+
+        // needs fixing
+        public int CompareTo(Information other)
+        {
+            if (other == null)
+            {
+                return 1;
+            }
+            return name.CompareTo(other.name);
+        }
     }
 }
